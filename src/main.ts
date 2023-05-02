a2fa88319d8760b0205fa7f53ab7c733193e01c9
Edward Arechwa <e.arechwa@nebucode.com>
2023-05-02 08:54:43 +0200

    ft: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Auth module for nestjs')
+    .setDescription('reusable module for nestjs applications')
+    .setVersion('1.0')
+    .addTag('auth')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/swagger', app, document);
 
   app.setGlobalPrefix('api');
   await app.listen(3000);
